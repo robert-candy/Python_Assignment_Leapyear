670e44df8dc2c9cb802004f70411263daac4afb3
robert-candy <candyrobertt@gmail.com>
2020-05-19 00:35:34 +0300

    first commit

--- Leapyer.py
+++ Leapyer.py
@@ -1,1 +1,10 @@
 
+num = int(input("please enter a year  :"))
+
+if (num % 4) == 0:
+    if (num % 100) == 0:
+        print(num, "is not a leap year")
+    else:
+       print(num, "is a leap year")
+else:
+  print(num, "is not a leap year")
